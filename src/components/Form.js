b8421104822962ba14c0ee21ef1b6a841d77c18d
xxcxxz <kulesza.michal@gmail.com>
2020-03-22 17:15:40 +0100

    Added formatCard()

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,38 @@
 import React from "react";
 import "./Form.scss";
 
-const Form = () => {
+const Form = ({ formatCard, ccNum, setCcNum, maxLength }) => {
+  const handleOnChangeCardNumber = e => {
+    setCcNum(formatCard(e.target.value));
+  };
+
   return (
     <div className="form-wrapper">
       <div className="form-row row-1">
         <label htmlFor="cardNumber">Card Number</label>
-        <input type="text" id="cardNumber" class="card-number-input" />
+        <input
+          type="text"
+          id="cardNumber"
+          onChange={handleOnChangeCardNumber}
+          value={ccNum}
+          maxLength={maxLength}
+          className="card-number-input"
+        />
       </div>
       <div className="form-row">
         <label htmlFor="cardHolder">Card Holders Name</label>
-        <input type="text" id="cardHolder" class="card-holder-input" />
+        <input type="text" id="cardHolder" className="card-holder-input" />
       </div>
       <div className="form-row row-3">
         <div className="card-expires">
           <label htmlFor="cardExpires">Expiration Date</label>
           <div className="card-expires-selection">
-            <select id="cardMonth" class="card-expires-month">
+            <select id="cardMonth" className="card-expires-month">
               <option value disabled="disabled" selected="selected">
                 Month
               </option>
             </select>
-            <select id="cardYear" class="card-expires-year">
+            <select id="cardYear" className="card-expires-year">
               <option value disabled="disabled" selected="selected">
                 Year
               </option>
@@ -30,7 +41,7 @@ const Form = () => {
         </div>
         <div className="card-cvv">
           <label htmlFor="cardCvv">CVV</label>
-          <input type="text" id="cardCvv" class="card-holder-cvv" />
+          <input type="text" id="cardCvv" className="card-holder-cvv" />
         </div>
       </div>
       <div className="form-row">4</div>
--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainCard.scss";
 
 import Card from "./Card";
 import Form from "./Form";
 
 const MainCard = () => {
+  const [ccNum, setCcNum] = useState(null);
+  const [maxLength, setMaxLength] = useState(null);
+
+  const formatCard = cardNumber => {
+    const num = cardNumber.toString().replace(/\D/g, "");
+    let formattedNum;
+
+    //Amex(15)
+    if (/^3[47]\d{0,13}$/.test(num)) {
+      formattedNum = num.replace(/(\d{4})/, "$1 ").replace(/(\d{4}) (\d{6})/, "$1 $2 ");
+      setMaxLength(17);
+    } else if (/^3(?:0[0-5]|[68]\d)\d{0,11}$/.test(num)) {
+      //Diners(14)
+      formattedNum = num.replace(/(\d{4})/, "$1 ").replace(/(\d{4}) (\d{6})/, "$1 $2 ");
+      setMaxLength(16);
+    } else if (/^\d{0,16}$/.test(num)) {
+      //Regular(16)
+      formattedNum = num
+        .replace(/(\d{4})/, "$1 ")
+        .replace(/(\d{4}) (\d{4})/, "$1 $2 ")
+        .replace(/(\d{4}) (\d{4}) (\d{4})/, "$1 $2 $3 ");
+      setMaxLength(19);
+    }
+    return formattedNum;
+  };
+
   return (
     <div className="main-card-wrapper">
       <Card></Card>
-      <Form></Form>
+      <Form formatCard={formatCard} setCcNum={setCcNum} ccNum={ccNum} maxLength={maxLength}></Form>
     </div>
   );
 };
